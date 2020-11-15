86226d2a1cf4380b91cca5e15601a255be23c721
siddharth2463 <73532986+siddharth2463@users.noreply.github.com>
2020-11-15 17:54:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box1,box2;
+function setup() {
+  
+  createCanvas(800,400);
+ box1 = createSprite(400, 200,67,99);
+ box2 = createSprite(200,100,50,50)
+ box1.shapeColor="green"
+ box2.shapeColor="green";
+ box1.debug=true;
+ box2.debug=true;
+}
+
+function draw() {
+  background(0);
+  box1.x=mouseX;
+  box1.y=mouseY;  
+  if(box1.x-box2.x<box1.width/2+box2.width/2
+    &&box2.x-box1.x<box1.width/2+box2.width/2
+    && box1.y-box2.y<box1.height/2+box2.height/2
+    &&box2.y-box1.y<box1.height/2+box2.height/2){
+    box1.shapeColor="red";
+    box2.shapeColor="red";
+  }
+  else{
+    box1.shapeColor="green"
+ box2.shapeColor="green";
+  }
+  drawSprites();
+}
